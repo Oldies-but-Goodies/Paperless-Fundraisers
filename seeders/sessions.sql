cb4fe4f12933bd1cb6b4804b1494efaaea9ea51e
The Podz <stefanpodz@yahoo.com>
2021-06-12 11:08:59 -0600

    updated sessions.sql

--- seeders/sessions.sql
+++ seeders/sessions.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE "name";
+USE "name";
+
+
 CREATE TABLE `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `expires` int(11) unsigned NOT NULL,
