d66c1277d09199ccdbcb6f86df1ccc0fd2c16299
Jason Jones <jason.e.jones@gmail.com>
2021-06-14 19:21:30 -0400

    adding classing for bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import axios from 'axios';
-import { isNil } from 'lodash';
-import React, { useEffect } from 'react';
-import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './pages/home';
-import Login from './pages/login';
-import Signup from './pages/signUp';
-import { LOADING, SET_USER, UNSET_USER } from './store/actions';
-import { useStoreContext } from './store/store';
+import axios from "axios";
+import { isNil } from "lodash";
+import React, { useEffect } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signUp";
+import { LOADING, SET_USER, UNSET_USER } from "./store/actions";
+import { useStoreContext } from "./store/store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const history = useHistory();
@@ -16,13 +17,13 @@ const App = () => {
   useEffect(() => {
     dispatch({ type: LOADING });
 
-    axios.get('/api/users').then((response) => {
+    axios.get("/api/users").then((response) => {
       if (!isNil(response.data.user)) {
         dispatch({ type: SET_USER, user: response.data.user });
-        history.push('/');
+        history.push("/");
       } else {
         dispatch({ type: UNSET_USER });
-        history.push('/login');
+        history.push("/login");
       }
     });
   }, [dispatch, history]);
@@ -33,13 +34,13 @@ const App = () => {
 
       {state.user ? (
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path='/' component={Home} />
         </Switch>
       ) : (
         <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Redirect to="/login" />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
+          <Redirect to='/login' />
         </Switch>
       )}
     </div>
