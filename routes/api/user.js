6fa41b9199e37946afc6eea37c1a20556fad3ae3
Jason Jones <jason.e.jones@gmail.com>
2021-06-28 20:00:56 -0400

    minor route tweaks

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const crypto = require('crypto');
+const express = require("express");
+const crypto = require("crypto");
 
-const User = require('../../models').User;
-const passport = require('../../passport');
-const { isValidEmail, isValidPassword } = require('../../utilities/authUtils');
+const User = require("../../models").User;
+const passport = require("../../passport");
+const { isValidEmail, isValidPassword } = require("../../utilities/authUtils");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/:id", async (req, res) => {
   if (req.user) {
     const user = await User.findOne({
       where: {
@@ -20,13 +20,13 @@ router.get('/', async (req, res) => {
       return;
     }
 
-    res.status(404).json({ status: 'error', message: 'User not found' });
+    res.status(404).json({ status: "error", message: "User not found" });
   } else {
     res.json({ user: null });
   }
 });
 
-router.post('/signup', async function (req, res, next) {
+router.post("/signup", async function (req, res, next) {
   let user = {};
 
   user = await User.findOne({
@@ -34,24 +34,26 @@ router.post('/signup', async function (req, res, next) {
       email: req.body.email,
     },
   });
-  console.log(user, "user")
+  console.log(user, "user");
   if (user) {
-    console.log('1')
-    res
-      .status(400)
-      .json({ message: `Sorry, a user is already using that email: ${req.body.email}` });
+    console.log("1");
+    res.status(400).json({
+      message: `Sorry, a user is already using that email: ${req.body.email}`,
+    });
     return;
   }
 
   if (!isValidPassword(req.body.password)) {
-    return res
-      .status(400)
-      .json({ status: 'error', message: 'Password must be 8 or more characters.' });
+    return res.status(400).json({
+      status: "error",
+      message: "Password must be 8 or more characters.",
+    });
   }
   if (!isValidEmail(req.body.email)) {
-    return res
-      .status(400)
-      .json({ status: 'error', message: 'Email address not formed correctly.' });
+    return res.status(400).json({
+      status: "error",
+      message: "Email address not formed correctly.",
+    });
   }
 
   try {
@@ -59,41 +61,44 @@ router.post('/signup', async function (req, res, next) {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       email: req.body.email,
-      role: 'user',
+      role: "user",
       password: req.body.password,
     });
   } catch (err) {
     console.log(err);
-    return res.json({ status: 'error', message: 'Email address already exists.' });
+    return res.json({
+      status: "error",
+      message: "Email address already exists.",
+    });
   }
 
   if (user) {
-    passport.authenticate('local', function (err, user, info) {
+    passport.authenticate("local", function (err, user, info) {
       if (err) {
         return next(err);
       }
       if (!user) {
-        return res.json({ status: 'error', message: info.message });
+        return res.json({ status: "error", message: info.message });
       }
 
       req.logIn(user, function (err) {
         if (err) {
           return next(err);
         }
-        return res.json({ status: 'ok' });
+        return res.json({ status: "ok" });
       });
     })(req, res, next);
   }
 });
 
-router.post('/login', function (req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
     if (err) {
       return next(err);
     }
 
     if (!user) {
-      return res.json({ status: 'error', message: info.message });
+      return res.json({ status: "error", message: info.message });
     }
 
     req.logIn(user, function (err) {
@@ -101,15 +106,15 @@ router.post('/login', function (req, res, next) {
         return next(err);
       }
 
-      return res.json({ status: 'ok', ...user });
+      return res.json({ status: "ok", ...user });
     });
   })(req, res, next);
 });
 
-router.get('/logout', function (req, res) {
+router.get("/logout", function (req, res) {
   req.session.destroy(() => {
-    res.redirect('/');
-  })
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
