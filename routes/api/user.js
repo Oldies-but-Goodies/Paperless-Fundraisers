816ba686270e925f60e1230920ddc21892ab12a7
Jason Jones <jason.e.jones@gmail.com>
2021-06-29 13:42:37 -0400

    getting ready for the update password - C.C. and I (ok - C.C.) tweaked the users route to add an updatePassword

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const crypto = require("crypto");
+const express = require('express');
+const crypto = require('crypto');
 
-const User = require("../../models").User;
-const passport = require("../../passport");
-const { isValidEmail, isValidPassword } = require("../../utilities/authUtils");
+const User = require('../../models').User;
+const passport = require('../../passport');
+const { isValidEmail, isValidPassword } = require('../../utilities/authUtils');
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   if (req.user) {
     const user = await User.findOne({
       where: {
@@ -20,13 +20,13 @@ router.get("/", async (req, res) => {
       return;
     }
 
-    res.status(404).json({ status: "error", message: "User not found" });
+    res.status(404).json({ status: 'error', message: 'User not found' });
   } else {
     res.json({ user: null });
   }
 });
 
-router.post("/signup", async function (req, res, next) {
+router.post('/signup', async function (req, res, next) {
   let user = {};
 
   user = await User.findOne({
@@ -34,9 +34,9 @@ router.post("/signup", async function (req, res, next) {
       email: req.body.email,
     },
   });
-  console.log(user, "user");
+  console.log(user, 'user');
   if (user) {
-    console.log("1");
+    console.log('1');
     res.status(400).json({
       message: `Sorry, a user is already using that email: ${req.body.email}`,
     });
@@ -45,14 +45,14 @@ router.post("/signup", async function (req, res, next) {
 
   if (!isValidPassword(req.body.password)) {
     return res.status(400).json({
-      status: "error",
-      message: "Password must be 8 or more characters.",
+      status: 'error',
+      message: 'Password must be 8 or more characters.',
     });
   }
   if (!isValidEmail(req.body.email)) {
     return res.status(400).json({
-      status: "error",
-      message: "Email address not formed correctly.",
+      status: 'error',
+      message: 'Email address not formed correctly.',
     });
   }
 
@@ -61,44 +61,44 @@ router.post("/signup", async function (req, res, next) {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       email: req.body.email,
-      role: "user",
+      role: 'user',
       password: req.body.password,
     });
   } catch (err) {
     console.log(err);
     return res.json({
-      status: "error",
-      message: "Email address already exists.",
+      status: 'error',
+      message: 'Email address already exists.',
     });
   }
 
   if (user) {
-    passport.authenticate("local", function (err, user, info) {
+    passport.authenticate('local', function (err, user, info) {
       if (err) {
         return next(err);
       }
       if (!user) {
-        return res.json({ status: "error", message: info.message });
+        return res.json({ status: 'error', message: info.message });
       }
 
       req.logIn(user, function (err) {
         if (err) {
           return next(err);
         }
-        return res.json({ status: "ok" });
+        return res.json({ status: 'ok' });
       });
     })(req, res, next);
   }
 });
 
-router.post("/login", function (req, res, next) {
-  passport.authenticate("local", function (err, user, info) {
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', function (err, user, info) {
     if (err) {
       return next(err);
     }
 
     if (!user) {
-      return res.json({ status: "error", message: info.message });
+      return res.json({ status: 'error', message: info.message });
     }
 
     req.logIn(user, function (err) {
@@ -106,15 +106,43 @@ router.post("/login", function (req, res, next) {
         return next(err);
       }
 
-      return res.json({ status: "ok", ...user });
+      return res.json({ status: 'ok', ...user });
     });
   })(req, res, next);
 });
 
-router.get("/logout", function (req, res) {
-  req.session.destroy(() => {
-    res.redirect("/");
-  });
+router.get('/logout', function (req, res) {
+  console.log('logout');
+  req.logOut();
+  // res.sendStatus(200);
+  res.redirect('/');
+});
+
+router.put('/updatePassword', (req, res, next) => {
+  passport.authenticate('local', async function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.json({ status: 'error', message: info.message });
+    }
+
+    const userData = await User.update(
+      {
+        password: req.body.nextPassword,
+      },
+      {
+        where: {
+          id: req.user.id,
+        },
+      }
+    );
+
+    console.log(userData);
+
+    res.json(userData);
+  })(req, res, next);
 });
 
 module.exports = router;
