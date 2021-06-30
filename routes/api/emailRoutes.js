dc5a86a3f6a22aed5c18b69dc2eadbe57f2780d8
The Podz <stefanpodz@yahoo.com>
2021-06-30 19:29:17 -0400

    insomnia routes from 06-30-21

--- routes/api/emailRoutes.js
+++ routes/api/emailRoutes.js
@@ -5,11 +5,13 @@ const email = require("../../utilities/email");
 // TODO add withAuth
 router.post("/", async (req, res) => {
   try {
+      console.log("mailer", req.body);
     const mail = await email(
       req.body.emailTo,
       req.body.emailSubject,
       req.body.emailBody
     );
+    console.log(mail);
     res.status(200).json(mail);
   } catch (e) {
     console.log(e);
