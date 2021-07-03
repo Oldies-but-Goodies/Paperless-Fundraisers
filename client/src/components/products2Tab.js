3b98a84ffffdd36f2614351429888ef62cf3e8b2
Jason Jones <jason.e.jones@gmail.com>
2021-07-03 14:31:45 -0400

    next-level tables.  Cell Editing, field validation, rudementory sort.  check it out.  Still needs to be hooked in to the UPDATE api...  Caleb?  :-)

--- client/src/components/products2Tab.js
+++ client/src/components/products2Tab.js
@@ -5,8 +5,14 @@ import API from '../lib/API';
 import EditProductModal from './editProductModal';
 import { useStoreContext } from '../store/store';
 import BootstrapTable from 'react-bootstrap-table-next';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+import filterFactory, {
+  textFilter,
+  numberFilter,
+} from 'react-bootstrap-table2-filter';
+import PropTypes from 'prop-types';
 
-const ProductsTab = () => {
+const Products2Tab = () => {
   const [state, dispatch] = useStoreContext();
   const [products, setProducts] = useState([
     { name: '', price: '', description: '', active: '' },
@@ -17,6 +23,69 @@ const ProductsTab = () => {
   const [toggleRender, setToggleRender] = useState(false);
 
   const [productIndex, setProductIndex] = useState(0);
+
+  const columns = [
+    {
+      dataField: 'id',
+      text: 'Product ID',
+      sort: true,
+      type: 'number',
+      // filter: numberFilter(),
+    },
+    {
+      dataField: 'name',
+      text: 'Product Name',
+      sort: true,
+      validator: (newValue, row, column) => {
+        if (newValue.length === 0) {
+          return {
+            valid: false,
+            message: 'Must have a Product Name',
+          };
+        }
+      },
+    },
+    {
+      dataField: 'description',
+      text: 'Description',
+      sort: true,
+    },
+    {
+      dataField: 'price',
+      text: 'Product Price',
+      sort: true,
+      type: 'number',
+      validator: (newValue, row, column) => {
+        if (isNaN(newValue)) {
+          return {
+            valid: false,
+            message: 'Price should be numeric',
+          };
+        }
+      },
+
+      // filter: numberFilter(),
+    },
+    {
+      dataField: 'active',
+      text: 'Active/Hidden',
+      sort: true,
+    },
+  ];
+
+  const defaultSorted = [
+    {
+      dataField: 'name',
+      order: 'desc',
+      type: 'number',
+    },
+  ];
+
+  // const cellEdit = {
+  //   afterSaveCell: (oldValue, newValue, row, column) => {
+  //     console.log(oldValue, newValue, row, column);
+  //   },
+  // };
 
   const getProductData = async () => {
     const productData = await API.Products.getAdminAllForFundraiser(
@@ -54,7 +123,22 @@ const ProductsTab = () => {
         showEdit={showEdit}
         setShowEdit={setShowEdit}
       />
-      <Table striped bordered hover className='mt-3'>
+      {/* https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/basic-celledit.html */}
+      <BootstrapTable
+        keyField='id'
+        data={products}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        noDataIndication='No products defined'
+        cellEdit={cellEditFactory({ mode: 'click' })}
+        // afterSaveCell={cellEdit.afterSaveCell()}
+        // filter={filterFactory()}
+        striped
+        hover
+        condensed
+        bootstrap4
+      />
+      {/* <Table striped bordered hover className='mt-3'>
         <thead>
           <tr>
             <th>Product</th>
@@ -77,9 +161,15 @@ const ProductsTab = () => {
             </tr>
           ))}
         </tbody>
-      </Table>
+      </Table> */}
     </Container>
   );
 };
 
-export default ProductsTab;
+Products2Tab.propTypes = {
+  name: PropTypes.string,
+  id: PropTypes.number,
+  price: PropTypes.number,
+};
+
+export default Products2Tab;
