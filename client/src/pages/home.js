c517ec422d2dea88ad6f4c105a0cb25a771ce3ac
JWalters <joshwalters34@gmail.com>
2021-07-10 14:51:27 -0400

    got order detail modal working

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Table } from "react-bootstrap"
-import API from '../lib/API';
+import { Container, Table } from "react-bootstrap";
+import API from "../lib/API";
 import { useStoreContext } from "../store/store";
-import OrderDetailModal from "../components/orderDetailModal"
+import OrderDetailModal from "../components/orderDetailModal";
 
-import BootstrapTable from 'react-bootstrap-table-next';
-import cellEditFactory from 'react-bootstrap-table2-editor';
+import BootstrapTable from "react-bootstrap-table-next";
+import cellEditFactory from "react-bootstrap-table2-editor";
 
 const Home = (props) => {
   const [state, dispatch] = useStoreContext();
 
   const [fundraiser, setFundraiser] = useState([]);
   const [orders, setOrders] = useState([]);
-  const [totalFundraiserSales, setTotalFundraiserSales] = useState("$xx.xx")
+  const [totalFundraiserSales, setTotalFundraiserSales] = useState("$xx.xx");
 
   const [order, setOrder] = useState(null);
 
@@ -27,171 +27,143 @@ const Home = (props) => {
 
   const columns = [
     {
-      dataField: 'id',
-      text: 'Order ID',
+      dataField: "id",
+      text: "Order ID",
       sort: true,
-      type: 'number',
-      editable: false
+      type: "number",
+      editable: false,
     },
     {
-      dataField: 'id',
-      text: 'Order ID',
+      dataField: "id",
+      text: "Order ID",
       sort: true,
-      type: 'number',
-      editable: false
+      type: "number",
+      editable: false,
     },
     {
-      dataField: 'Customer.first_name',
-      text: 'Customer First Name',
+      dataField: "Customer.first_name",
+      text: "Customer First Name",
       sort: true,
       editable: false,
     },
     {
-      dataField: 'Customer.last_name',
-      text: 'Customer Last Name',
+      dataField: "Customer.last_name",
+      text: "Customer Last Name",
       sort: true,
-      editable: false
+      editable: false,
     },
     {
-      dataField: 'order_total',
-      text: 'Total Sale',
+      dataField: "order_total",
+      text: "Total Sale",
       sort: true,
-      type: 'number',
-      editable: false
+      type: "number",
+      editable: false,
     },
     {
-      dataField: 'customer_remit',
-      text: 'Customer Paid',
-      sort: true
-    },
-    {
-      dataField: 'seller_remit',
-      text: 'Admin Paid',
+      dataField: "customer_remit",
+      text: "Customer Paid",
       sort: true,
-      editable: false
-    },
-  ]
-
-  const subColumns = [
-    {
-      dataField: 'Customer.first_name',
-      text: 'Customer First Name',
-      sort: true
     },
     {
-      dataField: 'Customer.last_name',
-      text: 'Customer Last Name',
-      sort: true
+      dataField: "seller_remit",
+      text: "Admin Paid",
+      sort: true,
+      editable: false,
     },
-    // {
-    //   dataField: 'Order_Details.Product.name',
-    //   text: 'Product',
-    //   sort: true
-    // },
-  ]
+  ];
 
-const getCurrentundraiser = async () => {
-  const fundraiserData = await API.Fundraisers.getCurrentFundraiser(state.currentFundraiser)
-  // console.log(fundraiserData);
-  setFundraiser(fundraiserData.data.fundraiserData);
-  setTotalFundraiserSales(fundraiserData.data.totalFundraiserSales)
-};
-
-const myOrders = async () => {
-  const myOrderData = await API.Orders.loggedInOrders(state.user.id);
-  console.log(myOrderData);
-  setOrders(myOrderData.data)
-}
-
-// let orderId = state.id;
-
-const handleCellEdit = async (oldValue, newValue, row, column) => {
-  const updateBodyObj = {
-    id: row.id,
-    first_name: row.Customer.first_name,
-    last_name: row.Customer.last_name,
-    order_total: row.order_total,
-    customer_remit: row.customer_remit,
-    seller_remit: row.seller_remit,
+  const getCurrentundraiser = async () => {
+    const fundraiserData = await API.Fundraisers.getCurrentFundraiser(
+      state.currentFundraiser
+    );
+    // console.log(fundraiserData);
+    setFundraiser(fundraiserData.data.fundraiserData);
+    setTotalFundraiserSales(fundraiserData.data.totalFundraiserSales);
   };
-  setErrorMsg(null);
 
-  try {
-    // const updateOrders = async () => {
-      
-      const myOrderData = await API.Orders.updateOrder(updateBodyObj.id, updateBodyObj);
-      console.log(updateBodyObj);
-      // setOrders(myOrderData.data)
-    // }
-    setErrorMsg('Order Updated');
+  const myOrders = async () => {
+    const myOrderData = await API.Orders.loggedInOrders(state.user.id);
+    console.log(myOrderData);
+    setOrders(myOrderData.data);
+  };
 
-    setTimeout(() => {
-      setErrorMsg(null);
-    }, 3000);
-  } catch (err) {
-    setErrorMsg(err.message);
-  }
-};
+  const handleCellEdit = async (oldValue, newValue, row, column) => {
+    const updateBodyObj = {
+      id: row.id,
+      first_name: row.Customer.first_name,
+      last_name: row.Customer.last_name,
+      order_total: row.order_total,
+      customer_remit: row.customer_remit,
+      seller_remit: row.seller_remit,
+    };
+    setErrorMsg(null);
 
+    try {
+      const myOrderData = await API.Orders.updateOrder(
+        updateBodyObj.id,
+        updateBodyObj
+      );
 
-const getOrderDetails = async () => {
-  const orderDetailsData = await API.OrderDetails.orderDetails(state.id);
-  // let orderId=order.id
-  setOrder(orderDetailsData.data);
-  console.log(orderDetailsData.data);
-};
+      setErrorMsg("Order Updated");
 
-useEffect(() => {
-  getCurrentundraiser();
-  myOrders()
-}, [])
-const handleRowClick = async (i) => {
-  setShowEdit(true);
-  setOrderIndex(i);
-  // <OrderDetailModal />
-};
+      setTimeout(() => {
+        setErrorMsg(null);
+      }, 3000);
+    } catch (err) {
+      setErrorMsg(err.message);
+    }
+  };
 
-// const rowEvents = {
-//   onClick: (e, row, ) => {
-//     <OrderDetailModal
-//                   orderId={order.id}
-//                 />;
-//   },
+  // const getOrderDetails = async () => {
+  //   const orderDetailsData = await API.OrderDetails.orderDetails(state.id);
+  //   // let orderId=order.id
+  //   setOrder(orderDetailsData.data);
+  //   console.log(orderDetailsData.data);
+  // };
 
-// const expandRow = {
-//   renderer: row => (
-//     <BootstrapTable 
-//     keyField='id'
-//     data={orders}
-//     columns={subColumns}
-//     />
+  useEffect(() => {
+    getCurrentundraiser();
+    myOrders();
+  }, []);
 
-//   )
-// };
+  const rowEvents = {
+    onClick: (e, row, rowIndex) => {
+      console.log(row, rowIndex);
+      setOrder(row);
+      setShowEdit(true);
+    },
+  };
 
   return (
-    <Container fluid className="homeContainer">
-   
-      <div className="row my-2  text-center">
-      <h2 className="col">Welcome to {fundraiser.name} Fundraiser </h2>
+    <Container fluid className='homeContainer'>
+      <OrderDetailModal
+        orderId={order ? order.id : ""}
+        show={showEdit}
+        onClose={() => setShowEdit(false)}
+      />
+      
+      <div className='row my-2  text-center'>
+        <h2 className='col'>Welcome to {fundraiser.name} Fundraiser </h2>
       </div>
-      <div className="row my-2 text-center">
-      <p className="col">{fundraiser.description} </p>
+      <div className='row my-2 text-center'>
+        <p className='col'>{fundraiser.description} </p>
       </div>
-      <div className="row my-2 text-center">
-      <h5 className="col">Our goal is to raise ${fundraiser.goal} and so far we've raised ${totalFundraiserSales}</h5>
+      <div className='row my-2 text-center'>
+        <h5 className='col'>
+          Our goal is to raise ${fundraiser.goal} and so far we've raised $
+          {totalFundraiserSales}
+        </h5>
       </div>
-    
       <BootstrapTable
         keyField='id'
         data={orders}
         columns={columns}
         // expandRow={ expandRow }
-        // rowEvents={ rowEvents}
+        rowEvents={rowEvents}
         // defaultSorted={defaultSorted}
         noDataIndication='No products defined'
         cellEdit={cellEditFactory({
-          mode: 'click',
+          mode: "click",
           afterSaveCell: (oldValue, newValue, row, column) => {
             handleCellEdit(oldValue, newValue, row, column);
           },
@@ -204,8 +176,7 @@ const handleRowClick = async (i) => {
         bootstrap4
         blurToSave
       />
-   
-{/* table only shows if user is non-Admin */}
+      {/* table only shows if user is non-Admin */}
       {/* <Table striped bordered hover>
         <thead>
           <tr>
@@ -236,7 +207,6 @@ const handleRowClick = async (i) => {
            
           </tbody>
       </Table> */}
-    
     </Container>
   );
 };
